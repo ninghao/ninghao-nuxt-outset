0e8fe5f30de7347ab23f968f7969e84799585c9a
wanghao8080 <wanghao@ninghao.net>
2023-06-28 14:31:15 +0800

    创建品牌后更新列表

--- src/stores/brand/brand-create.ts
+++ src/stores/brand/brand-create.ts
@@ -1,75 +1,73 @@
-import {
-  CreateBrandDto,
-  createBrandDtoSchema,
-} from '../../schema/brand';
+import { CreateBrandDto, createBrandDtoSchema } from '../../schema/brand';
 
 /**
  * BrandCreateStore
  */
-export const useBrandCreateStore = defineStore(
-  'brandCreate',
-  () => {
-    /**
-     * State
-     */
+export const useBrandCreateStore = defineStore('brandCreate', () => {
+  /**
+   * State
+   */
 
-    const _brand = {
-      name: '',
-      title: '',
-      alias: '',
-      logo: '',
+  const _brand = {
+    name: '',
+    title: '',
+    alias: '',
+    logo: '',
+  };
+
+  const brand = ref<CreateBrandDto>({ ..._brand });
+
+  /**
+   * Getters
+   */
+
+  /**
+   * Actions
+   */
+
+  const $reset = () => {
+    brand.value = { ..._brand };
+  };
+
+  const createBrand = async () => {
+    // 验证数据
+    createBrandDtoSchema.parse(brand.value);
+
+    // 主体数据
+    const body = {
+      ...brand.value,
     };
 
-    const brand = ref<CreateBrandDto>({ ..._brand });
+    // 请求接口
+    const { data, error } = await useFetch('/api/brands', {
+      method: 'POST',
+      body,
+      ...useApiInterceptor(),
+    });
 
-    /**
-     * Getters
-     */
+    // 处理错误
+    if (error.value) return;
 
-    /**
-     * Actions
-     */
+    // 重置状态
+    $reset();
 
-    const $reset = () => {
-      brand.value = { ..._brand };
-    };
+    // 显示通知
+    useToast().add({ title: '成功创建了品牌' });
 
-    const createBrand = async () => {
-      // 验证数据
-      createBrandDtoSchema.parse(brand.value);
+    // 更新列表
+    const store = useBrandIndexStore();
+    store.getBrands();
 
-      // 主体数据
-      const body = {
-        ...brand.value,
-      };
+    // 返回数据
+    return data;
+  };
 
-      // 请求接口
-      const { data, error } = await useFetch('/api/brands', {
-        method: 'POST',
-        body,
-        ...useApiInterceptor(),
-      });
-
-      // 处理错误
-      if (error.value) return;
-
-      // 重置状态
-      $reset();
-
-      // 显示通知
-      useToast().add({ title: '成功创建了品牌' });
-
-      // 返回数据
-      return data;
-    };
-
-    /**
-     * 返回值
-     */
-    return {
-      brand,
-      createBrand,
-      $reset,
-    };
-  },
-);
+  /**
+   * 返回值
+   */
+  return {
+    brand,
+    createBrand,
+    $reset,
+  };
+});
