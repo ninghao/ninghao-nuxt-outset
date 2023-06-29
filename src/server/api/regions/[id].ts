7f99a883dc949766832ae0b861747c6f993cb117
wanghao8080 <wanghao@ninghao.net>
2023-06-29 12:09:11 +0800

    定义获取区域接口

--- src/server/api/regions/[id].ts
+++ src/server/api/regions/[id].ts
@@ -1,8 +1,33 @@
-import { updateRegionDtoSchema } from '~/schema/region';
+import { updateRegionDtoSchema, Region } from '~/schema/region';
 
 export default defineEventHandler(async (event) => {
   const id = getRouterParam(event, 'id');
   const method = getMethod(event);
+
+  /**
+   * 获取区域
+   */
+  if (method === 'GET') {
+    // 检查用户身份
+    authGuard(event);
+
+    // 检查用户权限
+    if (event.context.ability.cannot('read', 'Region')) {
+      forbiddenException();
+    }
+
+    const [{ result }] = await surreal.query<[Array<Region>]>(
+      `
+        SELECT * 
+        FROM region
+        WHERE id = $id
+        FETCH brand;
+      `,
+      { id },
+    );
+
+    return result![0];
+  }
 
   /**
    * 更新区域
