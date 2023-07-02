8f25eb17079e851626fda3e7552eeb8302769cb5
wanghao8080 <wanghao@ninghao.net>
2023-07-02 10:41:05 +0800

    定义 Category Schema

---
+++ src/schema/category.ts
@@ -0,0 +1,24 @@
+import { z } from 'zod';
+
+/**
+ * 实体
+ */
+export const categorySchema = z.object({
+  id: z.string(),
+  title: z.string(),
+  alias: z.string(),
+  categoryId: z.string(),
+  url: z.string(),
+  brand: brandSchema,
+});
+
+/**
+ * 列表
+ */
+export const categoriesSchema = z.array(categorySchema);
+
+/**
+ * 类型
+ */
+export type Category = z.infer<typeof categorySchema>;
+export type Categories = z.infer<typeof categoriesSchema>;
