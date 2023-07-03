be66fcd6f2909c544af3cc9888fc1d18de68fc8d
wanghao8080 <wanghao@ninghao.net>
2023-07-03 11:22:47 +0800

    分页器样式

--- src/app.config.ts
+++ src/app.config.ts
@@ -46,10 +46,40 @@ const select = {
   },
 };
 
+const pagination = {
+  wrapper: 'flex items-center -space-x-px',
+  base: '',
+  rounded: 'first:rounded-s-md last:rounded-e-md',
+  default: {
+    size: 'sm',
+    activeButton: {
+      color: 'primary',
+      variant: 'soft',
+    },
+    inactiveButton: {
+      color: 'white',
+      variant: 'soft',
+    },
+    prevButton: {
+      color: 'white',
+      class: 'rtl:[&_span:first-child]:rotate-180',
+      icon: 'i-heroicons-chevron-left-20-solid',
+      variant: 'soft',
+    },
+    nextButton: {
+      color: 'white',
+      class: 'rtl:[&_span:last-child]:rotate-180',
+      icon: 'i-heroicons-chevron-right-20-solid ',
+      variant: 'soft',
+    },
+  },
+};
+
 export default {
   ui: {
     card,
     formGroup,
     select,
+    pagination,
   },
 };
