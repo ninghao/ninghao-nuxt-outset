10f1d24822b0c53b0f756c014d28d48d2aa0dd06
wanghao8080 <wanghao@ninghao.net>
2023-07-05 10:07:55 +0800

    改进登录表单设置 autocomplete

--- src/pages/signin/index.vue
+++ src/pages/signin/index.vue
@@ -1,24 +1,15 @@
 <template>
-  <div
-    :class="[
-      'flex flex-col items-center justify-center',
-      'min-h-screen',
-    ]"
-  >
-    <div
-      :class="[
-        'min-w-[400px]',
-        'border border-neutral-100 px-8 py-12 rounded-xl',
-      ]"
-    >
+  <div :class="['flex flex-col items-center justify-center', 'min-h-screen']">
+    <div :class="['min-w-[400px]', 'border border-neutral-100 px-8 py-12 rounded-xl']">
       <AppLogo class="mb-12" />
-      <div class="space-y-4 w-full">
+      <form class="space-y-4 w-full">
         <UFormGroup name="name" label="名字">
           <UInput
             v-model="store.name"
             size="lg"
             placeholder="请输入名字"
             variant="none"
+            autocomplete="username"
           />
         </UFormGroup>
         <UFormGroup name="title" label="密码">
@@ -28,13 +19,14 @@
             size="lg"
             variant="none"
             placeholder="请输入密码"
+            autocomplete="current-password"
           />
         </UFormGroup>
 
         <div class="border-t pt-6 border-neutral-100">
           <UButton size="lg" @click="store.signin">登录</UButton>
         </div>
-      </div>
+      </form>
     </div>
   </div>
 </template>
