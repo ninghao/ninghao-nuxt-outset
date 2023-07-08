4221f2eb7d8ff6f35743c4f1639f147025ab0434
wanghao8080 <wanghao@ninghao.net>
2023-07-08 09:06:08 +0800

    清理

--- src/components/console/brand/brand-create.vue
+++ src/components/console/brand/brand-create.vue
@@ -2,7 +2,7 @@
   <div class="px-8 py-8 space-y-4">
     <UFormGroup name="name" label="名称">
       <UInput
-        v-model="store.brand.name"
+        v-model="store.entity.name"
         size="lg"
         placeholder="请输入名称"
         variant="none"
@@ -10,7 +10,7 @@
     </UFormGroup>
     <UFormGroup name="title" label="标题">
       <UInput
-        v-model="store.brand.title"
+        v-model="store.entity.title"
         size="lg"
         variant="none"
         placeholder="请输入标题"
@@ -18,7 +18,7 @@
     </UFormGroup>
     <UFormGroup name="alias" label="中文名">
       <UInput
-        v-model="store.brand.alias"
+        v-model="store.entity.alias"
         size="lg"
         variant="none"
         placeholder="请输入中文名"
@@ -26,22 +26,18 @@
     </UFormGroup>
     <UFormGroup name="logo" label="标志">
       <UInput
-        v-model="store.brand.logo"
+        v-model="store.entity.logo"
         size="lg"
         variant="none"
         placeholder="请输入标志"
       />
     </UFormGroup>
     <div class="border-t pt-6 border-neutral-100">
-      <UButton size="lg" @click="store.createBrand"
-        >创建品牌</UButton
-      >
+      <UButton size="lg" @click="store.create">创建品牌</UButton>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-// const { brand, createBrand } = useCreateBrand();
-
-const store = useBrandCreateStore();
+const store = useBrandStore();
 </script>
